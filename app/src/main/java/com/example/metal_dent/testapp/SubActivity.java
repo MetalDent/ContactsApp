da329efbf1a4c5bb0a0b1e8a838c98535cce1208
MetalDent <priyapandya274@gmail.com>
2018-12-06 12:08:34 +0530

    Created SubActivity

---
+++ app/src/main/java/com/example/metal_dent/testapp/SubActivity.java
@@ -0,0 +1,22 @@
+package com.example.metal_dent.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+
+    TextView name, phone, address;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        phone = (EditText) findViewById(R.id.phone);
+
+        Intent intent = getIntent();
+    }
+}
