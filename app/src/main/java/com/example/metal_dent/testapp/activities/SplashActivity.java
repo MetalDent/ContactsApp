42512b26d569414655214e9cc91b44997e12e1e2
MetalDent <37808619+MetalDent@users.noreply.github.com>
2018-12-07 15:15:11 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/metal_dent/testapp/activities/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.metal_dent.testapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+import com.example.metal_dent.testapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
